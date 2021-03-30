4d1f0e50b4a5d94529ea0493766bf1f88cde95f7
hunter britten <huntergbritten@gmail.com>
2021-03-30 14:31:26 -0700

    app.js changes

--- bm.js
+++ bm.js
@@ -32,17 +32,6 @@ function getUserName() {
     }
 }
 
-function setUserName(event) {
-    event.preventDefault();
-    let stringifiedName = JSON.stringify(event.target.userName.value);
-    console.log('eventValue', event.target.userName.value);
-    localStorage.setItem('userName', stringifiedName);
- }
-
-const userNameFormSubmit = document.getElementById('userNameForm');
-userNameFormSubmit.addEventListener('submit', setUserName);
-console.log('username', userNameFormSubmit);
-
 //TODO initialize the health
 
 // function health() {
